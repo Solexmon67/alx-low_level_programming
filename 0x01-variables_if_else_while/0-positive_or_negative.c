8f83d33309694f9efa51e0c85f175dedd6f4c7e9
Solexmon67 <solexmon67@gmail.com>
2022-09-09 07:48:27 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,8 +11,7 @@
 int main(void)
 {
 	int n;
-	
-	srand(time(0));
+		srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
@@ -21,7 +20,5 @@ int main(void)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is negative\n", n);
-
 	return (0);
-
 }
