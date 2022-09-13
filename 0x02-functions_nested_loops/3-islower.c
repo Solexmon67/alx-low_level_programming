560b5777e7b95cf35e12c4f231e561ad00732faa
Solexmon67 <solexmon67@gmail.com>
2022-09-13 15:36:16 -0700

    okay

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,8 @@
 #include "main.h"
 
 /**
+ * _islower - Check Mai
+ * @c: An input character
  * Description: function uses _putchar function to print
  * alphabet in lowercase 10 times
  * Return: 1 if is lowercase or 0 if is uppercase
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,8 @@
 #include "main.h"
 
 /**
+ * _isalpha - Check Main
+ * @c: An input character
  * Description: function returns 1 if the character is a
  * letter, lowercase or uppercase.
  * Return: 1 or 0 in otherwise.
