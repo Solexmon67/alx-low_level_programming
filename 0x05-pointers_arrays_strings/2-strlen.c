1b5736954cfbe4f09de22ae44fa8ddcfe40e0a50
Solexmon67 <solexmon67@gmail.com>
2022-09-19 15:58:14 -0700

    working

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @str: The string to get the length of.
+ *
+ * Return: The length of @str.
+ */
+int _strlen(char *sili)
+{
+	int count;
+	
+	for (count = 0; *s != '\0'; ++s)
+		count++;	
+	
+	return (count);
+}
